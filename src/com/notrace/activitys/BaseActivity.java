bc6b40fcc902970b76944798b7ccd10c2c6ddd7f
messnoTrace <cy_nforget@126.com>
2015-05-20 17:42:44 +0800

    沉浸式菜单栏

--- src/com/notrace/activitys/BaseActivity.java
+++ src/com/notrace/activitys/BaseActivity.java
@@ -1,22 +1,54 @@
 package com.notrace.activitys;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.notrace.R;
+import com.notrace.immersivestatusbar.SystemBarTintManager;
 import com.notrace.utils.AnimotionUtils;
 import com.notrace.utils.AppManager;
 
 public abstract class BaseActivity extends Activity {
 
+	
+	
+	//沉浸式菜单栏
+	protected SystemBarTintManager mTintManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_base);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			setTranslucentStatus(true);
+		}
+		mTintManager = new SystemBarTintManager(this);
+		mTintManager.setStatusBarTintEnabled(true);
+		//设置App的主体颜色
+//		mTintManager.setStatusBarTintResource(R.color.navigation_bg);
+		
+		
 //		AppManager.addActivity(this);
 
 	}
+	
+	@TargetApi(19)
+	protected void setTranslucentStatus(boolean on) {
+		Window win = getWindow();
+		WindowManager.LayoutParams winParams = win.getAttributes();
+		final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+		if (on) {
+			winParams.flags |= bits;
+		} else {
+			winParams.flags &= ~bits;
+		}
+		win.setAttributes(winParams);
+	}
+	
 
 	@Override
 	public void startActivity(Intent intent) {
